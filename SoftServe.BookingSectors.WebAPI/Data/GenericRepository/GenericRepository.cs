afc3183848322d0715404b83530c346e9cf42861
macbook <macbook@MACBOOK7418>
2019-11-27 17:48:54 +0200

    Add Generic Repository

---
+++ SoftServe.BookingSectors.WebAPI/Data/GenericRepository/GenericRepository.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using SoftServe.BookingSectors.WebAPI.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace SoftServe.BookingSectors.Data.GenericRepository
+{
+    public sealed class GenericRepository<T>: IDisposable, IGenericRepository<T> where T : class
+    {
+        private readonly BookingSectorContext myDataBase = null;
+        private readonly DbSet<T> table = null;
+
+        public GenericRepository()
+        {
+            this.myDataBase = new BookingSectorContext();
+            table = myDataBase.Set<T>();
+
+        }
+        public GenericRepository(BookingSectorContext myDataBase)
+        {
+            this.myDataBase = myDataBase;
+            table = myDataBase.Set<T>();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return table.ToList();
+        }
+
+        public T GetById(object id)
+        {
+            return table.Find(id);
+        }
+        public void Insert(T obj)
+        {
+            table.Add(obj);
+        }
+        public void Update(T obj)
+        {
+            table.Attach(obj);
+            myDataBase.Entry(obj).State = EntityState.Modified;
+        }
+
+        public void Delete(object id)
+        {
+
+            T existing = table.Find(id);
+            table.Remove(existing);
+            
+        }
+        public void Save()
+        {
+            myDataBase.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            myDataBase.Dispose();
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ SoftServe.BookingSectors.WebAPI/Data/GenericRepository/IGenericRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using SoftServe.BookingSectors.WebAPI.Data.Models;
+
+namespace SoftServe.BookingSectors.Data.GenericRepository
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll();
+        T GetById(object id);
+        void Insert(T obj);
+        void Update(T obj);
+        void Delete(object id);
+        void Save();
+    }
+}
